a69c478bcec1638d8b8ffed71b5e794c99d1b95a
Liudmyla <savinska.l.y@gmail.com>
2024-10-17 20:04:57 +0200

    added methods for the buttons page

--- tests/Tests.Utils.Swd/PageObjects/ButtonsPage.cs
+++ tests/Tests.Utils.Swd/PageObjects/ButtonsPage.cs
@@ -1,10 +1,13 @@
 using Tests.Utils.Swd.Attribute;
 using Tests.Utils.Swd.BaseElements;
+using Tests.Utils.Swd.Browser;
 
 namespace Tests.Utils.Swd.PageObjects;
 
 public class ButtonsPage: BasePage.BasePage
 {
+    public string Url => "https://demoqa.com/buttons";
+    
     [FindBy(XPath = "//h1[contains(text(),\"Buttons\")]")]
     private WebElement? ButtonsTitle { get; set; }
     
@@ -25,5 +28,31 @@ public class ButtonsPage: BasePage.BasePage
     
     [FindBy(XPath = "/html//p[@id='dynamicClickMessage']")]
     private WebElement? ClickMeMessage { get; set; }
+
+    public ButtonsPage OpenInBrowser (BrowserNames name, params string[] args)
+    {
+        OpenWith(name, args);
+        return this;
+    }
     
+    public ButtonsPage NavigateToPage()
+    {
+        NavigateTo(Url);
+        return this;
+    }
+    
+    public void DoubleClick()
+    {
+        DoubleClickButton.DoubleClick();
+    }
+    
+    public void RightClick()
+    {
+        RightClickButton.RightClick();
+    }
+    
+    public void ClickMe()
+    {
+        ClickMeButton.Click();
+    }
 }
