cfa07a9b7865200c38d9b2e2a272f486eeb853bf
Liudmyla <savinska.l.y@gmail.com>
2024-10-17 19:54:56 +0200

    Added methods to click the buttons

--- tests/Tests.Utils.Swd/PageObjects/ButtonsPage.cs
+++ tests/Tests.Utils.Swd/PageObjects/ButtonsPage.cs
@@ -5,12 +5,25 @@ namespace Tests.Utils.Swd.PageObjects;
 
 public class ButtonsPage: BasePage.BasePage
 {
+    [FindBy(XPath = "//h1[contains(text(),\"Buttons\")]")]
+    private WebElement? ButtonsTitle { get; set; }
+    
     [FindBy(XPath = "/html//button[@id='doubleClickBtn']")]
-    private Button DoubleClickButton { get; set; }
+    private Button? DoubleClickButton { get; set; }
+    
+    [FindBy(XPath = "/html//p[@id='doubleClickMessage']")]
+    private WebElement? DoubleClickMessage { get; set; }
     
     [FindBy(XPath = "/html//button[@id='rightClickBtn']")]
-    private Button RightClickButton { get; set; }
+    private Button? RightClickButton { get; set; }
+    
+    [FindBy(XPath = "/html//p[@id='rightClickMessage']")]
+    private WebElement? RightClickMessage { get; set; }
     
     [FindBy(ClassName = "btn btn-primary")]
-    private Button ClickMeButton { get; set; }
+    private Button? ClickMeButton { get; set; }
+    
+    [FindBy(XPath = "/html//p[@id='dynamicClickMessage']")]
+    private WebElement? ClickMeMessage { get; set; }
+    
 }
--- tests/Tests.Utils.Swd/PageObjects/RadioButtonPage.cs
+++ tests/Tests.Utils.Swd/PageObjects/RadioButtonPage.cs
@@ -1,10 +1,16 @@
 using Tests.Utils.Swd.Attribute;
 using Tests.Utils.Swd.BaseElements;
+using Tests.Utils.Swd.Browser;
 
 namespace Tests.Utils.Swd.PageObjects;
 
 public class RadioButtonPage: BasePage.BasePage
 {
+    public string Url => "https://demoqa.com/radio-button";
+    
+    [FindBy(XPath = "//h1[contains(text(),\"Radio Button\")]")]
+    private WebElement? RadioButtonTitle { get; set; }
+    
     [FindBy(CssSelector = "[for='yesRadio']")]
     private Button? YesRadioButton { get; set; }
     
@@ -14,5 +20,31 @@ public class RadioButtonPage: BasePage.BasePage
     [FindBy(CssSelector = "[for='noRadio']")]
     private Button? NoRadioButton { get; set; }
     
+    public RadioButtonPage OpenInBrowser (BrowserNames name, params string[] args)
+    {
+        OpenWith(name, args);
+        return this;
+    }
+    
+    public RadioButtonPage NavigateToPage()
+    {
+        NavigateTo(Url);
+        return this;
+    }
+
+    public void ClickYesRadioButton()
+    {
+        YesRadioButton.Click();
+    }
+
+    public void ClickNoRadioButton()
+    {
+        NoRadioButton.Click();
+    }
+
+    public void ClickImpressiveRadioButton()
+    {
+        ImpressiveRadioButton.Click();
+    }
     
 }
